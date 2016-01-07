afce1f4a83088e427c456ce9514735ac9c9d33a5
Noah Damiani <noahdamiani@gmail.com>
2016-01-07 18:05:57 -0500

    Data fixtures using JSON

---
+++ js/app.js
@@ -0,0 +1,51 @@
+var app = angular.module('app', [
+  'firebase',
+  'ngRoute',
+  'laundryControllers'
+  ]);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+    when('/jobs', {
+      templateUrl: 'partials/laundry-list.html',
+      controller: 'LaundryListCtrl'
+    }).
+    when('/jobs/:jobId', {
+      templateUrl: 'partials/laundry-job.html',
+      controller: 'LaundryJobCtrl'
+    }).
+    when('/new_job', {
+      templateUrl: 'partials/new_job.html',
+      controller: 'NewJobCtrl'
+    }).
+    otherwise({
+      redirectTo: '/jobs'
+    });
+  }]);
+
+app.controller('HomeController', function ($scope, $location) {
+  $scope.siteName = 'Angular Laundry';
+  $scope.version = '1.0';
+  $scope.go = function (path) {
+    $location.path(path);
+  };
+});
+
+app.controller('NewJobCtrl', function ($scope, $firebaseArray, $location) {
+  var fb = new Firebase("https://angular-laundry.firebaseio.com/jobs");
+  $scope.jobs = $firebaseArray(fb);
+  $scope.addJob = function() {
+    var date = new Date();
+    $scope.jobs.$add({
+      name: $scope.firstName,
+      phone: $scope.phone,
+      createdAt: date.toString(),
+      type: $scope.type,
+      status: {
+        details: 'not started',
+        done: false
+      }
+    });
+  };
+});
--- js/controllers.js
+++ js/controllers.js
@@ -1,38 +1,16 @@
-var app = angular.module('app', ['firebase']);
+var laundryControllers = angular.module('laundryControllers', []);
 
-app.controller('HomeController', function ($scope, $firebaseObject) {
-  var fb = new Firebase("https://angular-laundry.firebaseio.com");
-  var syncObj = $firebaseObject(fb);
+laundryControllers.controller('LaundryListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('/angular-laundry/data/fixtures.json').success(function(data) {
+      $scope.jobs = data;
+    });
+  }]);
 
-  syncObj.$bindTo($scope, "data");
-
-  //$scope.data = syncObj;
-  $scope.siteName = 'Angular Laundry';
-  $scope.version = '1.0';
-  $scope.linkItems = [
-    {'url': 'http://google.com',
-     'name': 'Google'},
-    {'url': 'http://firebase.io',
-     'name': 'Firebase'},
-    {'url': 'http://angularjs.org',
-     'name': 'AngularJS'}
-  ];
-
-  $scope.jobs = [
-    {'id': '1',
-    'time': '1:45pm',
-    'type': 'Normal Wash and Fold',
-    'name': 'Noah Damiani',
-    'status': 'Not Started'},
-    {'id': '2',
-    'time': '10:00am',
-    'type': 'Organic Wash and Fold',
-    'name': 'Patrick Wilson',
-    'status': 'completed'},
-    {'id': '3',
-    'time': '9:00am',
-    'type': 'Normal Wash and Fold',
-    'name': 'Gabby Namm',
-    'status': 'picked up'}
-  ];
-});
+laundryControllers.controller('LaundryJobCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    $scope.jobId = $routeParams.jobId;
+    $http.get('/angular-laundry/data/fixtures.json').success(function(data) {
+      $scope.job = data[$routeParams.jobId-1];
+    });
+  }]);
