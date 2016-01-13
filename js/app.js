3e23388aef21e677ce3422a969a318e34e40ad71
Noah Damiani <noahdamiani@gmail.com>
2016-01-12 21:59:10 -0500

    Reorganized controllers and made reusable firebase service!

--- js/app.js
+++ js/app.js
@@ -9,43 +9,48 @@ var app = angular.module('app', [
   ]);
 
 app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.otherwise('/');
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'views/home.html',
-        controller: 'HomeController'
-      })
-      .state('laundryList', {
-        url: '/jobs',
-        templateUrl: 'views/laundry-list/list.html',
-        controller: 'LaundryListCtrl'
-      })
-      .state('laundryJob', {
-        url: '/jobs/:jobId',
-        templateUrl: 'views/laundry-job/job.html',
-        controller: 'LaundryJobCtrl'
-      })
-      .state('editJob', {
-        url: '/jobs/:jobId/edit',
-        templateUrl: 'views/laundry-job/edit.html',
-        controller: 'LaundryJobEditCtrl'
-      })
-      .state('archives', {
-        url: '/archives',
-        templateUrl: 'views/archives/list.html',
-        controller: 'LaundryArchiveCtrl'
-      })
-      .state('archivedJob', {
-        url: '/archives/:archiveId',
-        templateUrl: 'views/archives/archived_job.html',
-        controller: 'LaundryArchivedJobCtrl'
-      })
-      .state('newJob', {
-        url: '/new-job',
-        templateUrl: 'views/laundry-job/new/new_job.html',
-        controller: 'NewJobCtrl'
-      });
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'views/home.html',
+    controller: 'HomeController'
+  })
+  .state('laundryList', {
+    url: '/jobs',
+    templateUrl: 'views/laundry-list/list.html',
+    controller: 'LaundryListCtrl'
+  })
+  .state('laundryJob', {
+    url: '/jobs/:jobId',
+    templateUrl: 'views/laundry-job/job.html',
+    controller: 'LaundryJobCtrl'
+  })
+  .state('editJob', {
+    url: '/jobs/:jobId/edit',
+    templateUrl: 'views/laundry-job/edit.html',
+    controller: 'LaundryJobEditCtrl'
+  })
+  .state('archives', {
+    url: '/archives',
+    templateUrl: 'views/archives/list.html',
+    controller: 'LaundryArchiveCtrl'
+  })
+  .state('archivedJob', {
+    url: '/archives/:archiveId',
+    templateUrl: 'views/archives/archived_job.html',
+    controller: 'LaundryArchivedJobCtrl'
+  })
+  .state('newJob', {
+    url: '/new-job',
+    templateUrl: 'views/laundry-job/new/new_job.html',
+    controller: 'NewJobCtrl'
+  })
+  .state('profile', {
+    url: '/profile',
+    templateUrl: 'views/user/profile.html',
+    controller: 'UserProfileCtrl'
+  });
 }]);
 
 app.factory('Auth', AuthService);
@@ -60,7 +65,6 @@ app.controller('HomeController', function ($scope, $location, $firebaseAuth, $ht
   $scope.version = '1.0';
   $scope.auth = Auth;
   $scope.loggedIn = false;
-
   $scope.auth.$onAuth(function(authData) {
     if (authData === null) {
     } else {
@@ -74,11 +78,13 @@ app.controller('HomeController', function ($scope, $location, $firebaseAuth, $ht
   });
 
   $http.get("https://api.github.com/repos/noahdamiani/angular-laundry/commits").then(function(response) {
-      $scope.commits = response.data.slice(0, 15);
+    $scope.commits = response.data.slice(0, 15);
   });
 
   $scope.login = function() {
-    fb.authWithOAuthPopup("facebook", function(error, authData) {});
+    fb.authWithOAuthPopup("facebook", function(error, authData) {
+      console.log(authData);
+    });
   };
 
   $scope.logout = function() {
@@ -86,50 +92,4 @@ app.controller('HomeController', function ($scope, $location, $firebaseAuth, $ht
     window.localStorage.removeItem("firebase:session::angular-laundry");
     location.reload();
   };
-});
-
-// FILTERS
-app.filter('tel', function () {
-    return function (tel) {
-        if (!tel) { return ''; }
-
-        var value = tel.toString().trim().replace(/^\+/, '');
-
-        if (value.match(/[^0-9]/)) {
-            return tel;
-        }
-
-        var country, city, number;
-
-        switch (value.length) {
-            case 10: // +1PPP####### -> C (PPP) ###-####
-                country = 1;
-                city = value.slice(0, 3);
-                number = value.slice(3);
-                break;
-
-            case 11: // +CPPP####### -> CCC (PP) ###-####
-                country = value[0];
-                city = value.slice(1, 4);
-                number = value.slice(4);
-                break;
-
-            case 12: // +CCCPP####### -> CCC (PP) ###-####
-                country = value.slice(0, 3);
-                city = value.slice(3, 5);
-                number = value.slice(5);
-                break;
-
-            default:
-                return tel;
-        }
-
-        if (country == 1) {
-            country = "";
-        }
-
-        number = number.slice(0, 3) + '-' + number.slice(3);
-
-        return (country + " (" + city + ") " + number).trim();
-    };
 });
---
+++ js/controllers/laundry.js
@@ -0,0 +1,60 @@
+var laundryControllers = angular.module('laundryControllers', []);
+
+laundryControllers.factory('FireBase', getFireBaseService);
+function getFireBaseService($firebaseArray, $firebaseObject) {
+  return {
+    getArray: function(url) {
+      var ref = new Firebase("https://angular-laundry.firebaseio.com" + url);
+      return $firebaseArray(ref);
+    },
+    getObject: function(url) {
+      var ref = new Firebase("https://angular-laundry.firebaseio.com" + url);
+      return $firebaseObject(ref);
+    }
+  }
+}
+
+laundryControllers.controller('NewJobCtrl', function ($scope, $location, Jobs) {
+  $scope.jobs = Jobs.getJobs();
+  $scope.addJob = function() {
+    var date = new Date();
+    $scope.jobs.$add({
+      name: $scope.jobName,
+      phone: $scope.phone,
+      createdAt: date.toString(),
+      type: $scope.type[0],
+      status: {
+        details: 'not started',
+        done: false
+      }
+    });
+    $location.path('/jobs');
+  };
+});
+
+laundryControllers.controller('LaundryListCtrl', function ($scope, $location, $stateParams, FireBase) {
+  $scope.jobs = FireBase.getArray('/jobs/');
+  $scope.archives = FireBase.getArray('/archives/');
+  $scope.archiveIt = function() {
+    $scope.archives.$add(this.job);
+    $scope.jobs.$remove(this.job);
+    $location.path('/archives');
+  };
+});
+
+laundryControllers.controller('LaundryJobCtrl', function ($scope, $location, $stateParams, FireBase) {
+  $scope.job = FireBase.getObject('/jobs/' + $stateParams.jobId);
+});
+
+laundryControllers.controller('LaundryJobEditCtrl', function ($scope, $stateParams, FireBase) {
+  var jobData = FireBase.getObject('/jobs/' + $stateParams.jobId);
+  jobData.$bindTo($scope, "job");
+});
+
+laundryControllers.controller('LaundryArchiveCtrl', function ($scope, FireBase) {
+  $scope.archives = FireBase.getArray('/archives');
+});
+
+laundryControllers.controller('LaundryArchivedJobCtrl', function ($scope, $stateParams, FireBase) {
+  $scope.archive = FireBase.getObject('/archive/' + $stateParams.archiveId);
+});
